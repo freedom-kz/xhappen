8f59068fcfbbaa030e8e87ba1495f44bb37f0561
kang.zou <kang.zou@foxmail.com>
2024-04-02 14:33:24 +0800

    feat

--- app/xjob/internal/biz/aliSms.go
+++ app/xjob/internal/biz/aliSms.go
@@ -15,6 +15,7 @@ import (
 )
 
 type AliSMSUseCase struct {
+	conf   *conf.Bootstrap
 	client *dysmsapi20170525.Client
 	log    *log.Helper
 }
@@ -34,6 +35,7 @@ func NewAliSMSUseCase(conf *conf.Bootstrap, logger log.Logger) *AliSMSUseCase {
 	}
 
 	return &AliSMSUseCase{
+		conf:   conf,
 		client: client,
 		log:    log.NewHelper(log.With(logger, "module", "usecase/alisms")),
 	}
@@ -41,10 +43,10 @@ func NewAliSMSUseCase(conf *conf.Bootstrap, logger log.Logger) *AliSMSUseCase {
 
 func (useCase *AliSMSUseCase) SendSMS(ctx context.Context, data *event.SMSCode) error {
 	sendReq := &dysmsapi20170525.SendSmsRequest{
-		PhoneNumbers:  nil,
-		SignName:      nil,
-		TemplateCode:  nil,
-		TemplateParam: nil,
+		PhoneNumbers:  &data.Mobile,
+		SignName:      &useCase.conf.Info.Sms.SignName,
+		TemplateCode:  &useCase.conf.Info.Sms.TemplateCode,
+		TemplateParam: &data.Authcode,
 	}
 
 	sendResp, _err := useCase.client.SendSms(sendReq)
@@ -57,99 +59,5 @@ func (useCase *AliSMSUseCase) SendSMS(ctx context.Context, data *event.SMSCode) 
 		useCase.log.Info("err:", tea.StringValue(sendResp.Body.Message))
 		return _err
 	}
-
 	return nil
 }
-
-// // This file is auto-generated, don't edit it. Thanks.
-// package main
-
-// import (
-//   "os"
-//   dysmsapi  "github.com/alibabacloud-go/dysmsapi-20170525/v2/client"
-//   openapi  "github.com/alibabacloud-go/darabonba-openapi/client"
-//   console  "github.com/alibabacloud-go/tea-console/client"
-//   env  "github.com/alibabacloud-go/darabonba-env/client"
-//   util  "github.com/alibabacloud-go/tea-utils/service"
-//   time  "github.com/alibabacloud-go/darabonba-time/client"
-//   string_  "github.com/alibabacloud-go/darabonba-string/client"
-//   "github.com/alibabacloud-go/tea/tea"
-// )
-
-// // 使用AK&SK初始化账号Client
-// func CreateClient (accessKeyId *string, accessKeySecret *string) (_result *dysmsapi.Client, _err error) {
-//   config := &openapi.Config{}
-//   config.AccessKeyId = accessKeyId
-//   config.AccessKeySecret = accessKeySecret
-//   _result = &dysmsapi.Client{}
-//   _result, _err = dysmsapi.NewClient(config)
-//   return _result, _err
-// }
-
-// func _main (args []*string) (_err error) {
-//   client, _err := CreateClient(env.GetEnv(tea.String("ACCESS_KEY_ID")), env.GetEnv(tea.String("ACCESS_KEY_SECRET")))
-//   if _err != nil {
-//     return _err
-//   }
-
-//   // 1.发送短信
-//   sendReq := &dysmsapi.SendSmsRequest{
-//     PhoneNumbers: args[0],
-//     SignName: args[1],
-//     TemplateCode: args[2],
-//     TemplateParam: args[3],
-//   }
-//   sendResp, _err := client.SendSms(sendReq)
-//   if _err != nil {
-//     return _err
-//   }
-
-//   code := sendResp.Body.Code
-//   if !tea.BoolValue(util.EqualString(code, tea.String("OK"))) {
-//     console.Log(tea.String("错误信息: " + tea.StringValue(sendResp.Body.Message)))
-//     return _err
-//   }
-
-//   bizId := sendResp.Body.BizId
-//   // 2. 等待 10 秒后查询结果
-//   _err = util.Sleep(tea.Int(10000))
-//   if _err != nil {
-//     return _err
-//   }
-//   // 3.查询结果
-//   phoneNums := string_.Split(args[0], tea.String(","), tea.Int(-1))
-//   for _, phoneNum := range phoneNums {
-//     queryReq := &dysmsapi.QuerySendDetailsRequest{
-//       PhoneNumber: util.AssertAsString(phoneNum),
-//       BizId: bizId,
-//       SendDate: time.Format(tea.String("yyyyMMdd")),
-//       PageSize: tea.Int64(10),
-//       CurrentPage: tea.Int64(1),
-//     }
-//     queryResp, _err := client.QuerySendDetails(queryReq)
-//     if _err != nil {
-//       return _err
-//     }
-
-//     dtos := queryResp.Body.SmsSendDetailDTOs.SmsSendDetailDTO
-//     // 打印结果
-//     for _, dto := range dtos {
-//       if tea.BoolValue(util.EqualString(tea.String(tea.ToString(tea.Int64Value(dto.SendStatus))), tea.String("3"))) {
-//         console.Log(tea.String(tea.StringValue(dto.PhoneNum) + " 发送成功，接收时间: " + tea.StringValue(dto.ReceiveDate)))
-//       } else if tea.BoolValue(util.EqualString(tea.String(tea.ToString(tea.Int64Value(dto.SendStatus))), tea.String("2"))) {
-//         console.Log(tea.String(tea.StringValue(dto.PhoneNum) + " 发送失败"))
-//       } else {
-//         console.Log(tea.String(tea.StringValue(dto.PhoneNum) + " 正在发送中..."))
-//       }
-
-//     }
-//   }
-//   return _err
-// }
-
-// func main() {
-//   err := _main(tea.StringSlice(os.Args[1:]))
-//   if err != nil {
-//     panic(err)
-//   }
-// }
--- app/xjob/internal/service/smsCodeReceiver.go
+++ app/xjob/internal/service/smsCodeReceiver.go
@@ -3,37 +3,40 @@ package service
 import (
 	"context"
 	"encoding/json"
+	"xhappen/app/xjob/internal/biz"
 	"xhappen/pkg/event"
 
 	"github.com/go-kratos/kratos/v2/log"
 )
 
 type SmsCodeReceiverService struct {
-	ctx      context.Context
-	receiver event.Receiver
-	log      *log.Helper
+	ctx           context.Context
+	aliSMSUseCase *biz.AliSMSUseCase
+	receiver      event.Receiver
+	log           *log.Helper
 }
 
-func NewSmsCodeReceiverService(ctx context.Context, receiver event.Receiver, logger log.Logger) *SmsCodeReceiverService {
+func NewSmsCodeReceiverService(ctx context.Context, receiver event.Receiver, aliSMSUseCase *biz.AliSMSUseCase, logger log.Logger) *SmsCodeReceiverService {
 	service := &SmsCodeReceiverService{
-		ctx:      ctx,
-		receiver: receiver,
-		log:      log.NewHelper(logger),
+		ctx:           ctx,
+		receiver:      receiver,
+		aliSMSUseCase: aliSMSUseCase,
+		log:           log.NewHelper(logger),
 	}
-	service.receiver.Receive(ctx, handler)
+	service.receiver.Receive(ctx, service.handler)
 	return service
 }
 
-func handler(ctx context.Context, msg event.Event) error {
-
+func (service *SmsCodeReceiverService) handler(ctx context.Context, msg event.Event) error {
 	data := msg.Value()
 	smsdataMsg := event.SMSCode{}
 	err := json.Unmarshal(data, &smsdataMsg)
-	if err != nil{
+	if err != nil {
 		return err
 	}
-
-	
-
+	err = service.aliSMSUseCase.SendSMS(ctx, &smsdataMsg)
+	if err != nil {
+		return err
+	}
 	return nil
 }
